e91653917db8771df0016c774c111c29da90f851
Nicole Plattner <nicole_plattner@yahoo.de>
2014-01-13 22:06:29 +0100

    Fixed all tests :-)

--- models/heating.rb
+++ models/heating.rb
@@ -1,2 +1,28 @@
 class Heating
+	#init heating
+	def initialize(name, area, temperature)
+        @name = name
+		@temperature = temperature
+		@area = area
+    end    
+	
+	#returns its name
+	def name()
+		return @name
+	end
+	
+	#returns its temperature
+	def temperature()
+		return @temperature
+	end
+		
+	#returns its area
+	def area()
+		return @area
+	end
+	
+	#returns the calculated efficiency per degree	
+	def efficiency()
+		return @area / @temperature
+	end
 end
