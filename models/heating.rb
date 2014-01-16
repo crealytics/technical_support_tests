364b64840c9aad8389c44ff5007478c849529e1b
Michael Mueller <michaelrudolf.mueller@wibizu.de>
2014-01-16 23:57:39 +0100

    No errors

--- models/heating.rb
+++ models/heating.rb
@@ -1,2 +1,13 @@
 class Heating
+  #Attributes for Heating class
+  attr_accessor :name, :area, :temperature, :efficiency
+  
+  #Heating class constructor
+  def initialize (name, area, temperature)
+    @name = name
+    @area = area
+    @temperature = temperature
+    @efficiency = area / temperature
+  end  
+  
 end
